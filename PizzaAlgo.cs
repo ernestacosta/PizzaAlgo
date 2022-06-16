6d88f442461bd11471178e3420645eaa4d1aee52
Ernest Acosta <ernestacosta@icloud.com>
2022-06-16 08:27:45 -0700

    Added step 1a

--- PizzaAlgo.cs
+++ PizzaAlgo.cs
@@ -1,6 +1,7 @@
-Decide which business to order from
-Decide which type of pizza to order
-Decide what toppings to add
-Place order for delivery
-Tip driver once pizza is delivered
-Eat and enjoy pizza
+1.  Decide which business to order from
+1a. Make sure to check reviews on yelp/google
+2.  Decide which type of pizza to order
+3.  Decide what toppings to add
+4.  Place order for delivery
+5.  Tip driver once pizza is delivered
+6.  Eat and enjoy pizza
